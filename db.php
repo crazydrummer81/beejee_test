eb5c0ea779f83261282af6591a4a267562d4898e
crazydrummer81 <crazy_drummer@mail.ru>
2020-04-16 14:58:34 +0600

    Изменил данные аккаунта БД для хостинга

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
         require "lib/rb-mysql.php";
-        R::setup( 'mysql:host=localhost;dbname=beejee',
-        'beejee', '1WJBykwc79KMtmOw' ); //for both mysql or mariaDB
+        R::setup( 'mysql:host=localhost;dbname=h-44856_beejee',
+        'h-44856_beejee', '1WJBykwc79KMtmOw' ); //for both mysql or mariaDB
 
         session_start();
 ?>
